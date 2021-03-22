f4f2433912f1d52dda72e9646e59302cbe8e5b27
SushilG96 <susgupta@redhat.com>
2021-03-22 15:52:14 +0530

    Update

--- findPeakElement.go
+++ findPeakElement.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-        "fmt"
-)
-
-func main() {
-    n := []int{1, 2, 1, 3, 5, 6, 4}
-    fmt.Println(findPeakElement(n))
-}
 /*
 A peak element is an element that is strictly greater than its neighbors.
 
@@ -22,13 +14,13 @@ Output: 2
 Explanation: 3 is a peak element and your function should return the index number 2.
 */
 func findPeakElement(nums []int) int {
-    m := -1; i := 0
-    for pos, x := range nums{
-        if x > m{
-            m = x
-            i = pos
-       }
-    }
-    return i
+	m := -1
+	i := 0
+	for pos, x := range nums {
+		if x > m {
+			m = x
+			i = pos
+		}
+	}
+	return i
 }
-
